5a6abff2c91026b23d18ee4f54f23c4f33c335b6
Jose Manuel Hernandez Mayorga <jose.mayorga@coppel.com>
2024-08-20 17:58:03 -0600

    Se crea package para ejemplo de clases abstractas

---
+++ Java POO/src/com/example/clases_abstractas/AbstractasEjemplo.java
@@ -0,0 +1,7 @@
+package com.example.clases_abstractas;
+
+public class AbstractasEjemplo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde el package de las clases abstractas!");
+    }
+}
